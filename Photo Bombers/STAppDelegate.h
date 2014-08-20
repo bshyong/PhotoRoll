fd353da0d219342d8c2bc5de7a785a186ee8852d
Benjamin Shyong <ben.shyong@gmail.com>
2014-08-19 20:17:00 -0700

    Initial Commit

---
+++ Photo Bombers/STAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STAppDelegate.h
+//  Photo Bombers
+//
+//  Created by Benjamin Shyong on 8/19/14.
+//  Copyright (c) 2014 ShyongTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
